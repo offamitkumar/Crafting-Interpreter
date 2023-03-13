add30b0327bc1fd06a57f59bc355bdf3c09e05b2
Amit Kumar <amitlocham09@icloud.com>
2023-03-13 21:31:41 +0530

    evaluating literals

--- Lox/src/com/craftinginterpreters/lox/Interpreter.java
+++ Lox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,4 +1,8 @@
 package com.craftinginterpreters.lox;
 
-public class Interpreter {
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
 }
